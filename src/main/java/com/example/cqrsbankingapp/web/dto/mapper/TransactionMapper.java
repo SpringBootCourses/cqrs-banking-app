95418ee17b2cdaf65b3817f622f013224c111098
Kolyuchkin Aleksandr <a.kolyuchkin@crpt.ru>
2024-02-26 15:55:28 +0300

    #28 TransactionMapper refactoring. Eliminated the comments of the review.

--- src/main/java/com/example/cqrsbankingapp/web/dto/mapper/TransactionMapper.java
+++ src/main/java/com/example/cqrsbankingapp/web/dto/mapper/TransactionMapper.java
@@ -7,9 +7,11 @@ import org.mapstruct.Mapper;
 import static org.mapstruct.InjectionStrategy.CONSTRUCTOR;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
-@Mapper(componentModel = SPRING,
+@Mapper(
+        componentModel = SPRING,
         uses = CardMapper.class,
-        injectionStrategy = CONSTRUCTOR)
+        injectionStrategy = CONSTRUCTOR
+)
 public interface TransactionMapper
         extends Mappable<Transaction, TransactionDto> {
 }
