652a28e66ecb5d21e6f368a772b5405fa22f42ee
Ilya Lisov <ilya.lisov.yt@gmail.com>
2024-04-08 14:07:40 +0300

    #61 Generate card data

--- event-handler/src/main/java/com/example/eventhandler/service/card/CardServiceImpl.java
+++ event-handler/src/main/java/com/example/eventhandler/service/card/CardServiceImpl.java
@@ -5,6 +5,9 @@ import com.example.common.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class CardServiceImpl implements CardService {
@@ -15,8 +18,31 @@ public class CardServiceImpl implements CardService {
     public Card create(
             final Card card
     ) {
-        //TODO set date cvv and number
+        card.setCvv(generateCvv());
+        card.setDate(generateDate());
+        card.setNumber(generateNumber());
         return repository.save(card);
+    }
+
+    private String generateCvv() {
+        return String.valueOf(100 + (int) (Math.random() * 899));
+    }
+
+    private String generateDate() {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate expirationDate = currentDate.plusYears(5);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yy");
+        return expirationDate.format(formatter);
+    }
+
+    private String generateNumber() {
+        return String.format(
+                "%04d%04d%04d%04d",
+                1000 + (int) (Math.random() * 8999),
+                1000 + (int) (Math.random() * 8999),
+                1000 + (int) (Math.random() * 8999),
+                1000 + (int) (Math.random() * 8999)
+        );
     }
 
 }
